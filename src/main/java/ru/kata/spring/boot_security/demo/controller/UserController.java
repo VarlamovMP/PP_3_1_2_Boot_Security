7105858723165569d138c8233f6a7c44ea36b163
Михаил <koko0104199@gmail.com>
2024-11-30 20:34:46 +0300

    2

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -0,0 +1,25 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.services.UserService;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping(value = "/user/")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String user(Model model, Principal principal) {
+        model.addAttribute("user", userService.findUserByUsername(principal.getName()));
+        return "user/user";
+    }
+}
