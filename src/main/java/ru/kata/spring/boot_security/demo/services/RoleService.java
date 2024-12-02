f1c9f34b2dc1502fd564dec86fbfff8e0be2dbff
Михаил <koko0104199@gmail.com>
2024-12-02 19:41:37 +0300

    4

--- src/main/java/ru/kata/spring/boot_security/demo/services/RoleService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/RoleService.java
@@ -1,29 +1,15 @@
 package ru.kata.spring.boot_security.demo.services;
 
-import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.models.Role;
-import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
-
 
 import java.util.List;
 
-@Service
-public class RoleService {
-    private final RoleRepository roleRepository;
+public interface RoleService {
+    List<Role> getListRoles();
 
-    public RoleService(RoleRepository roleRepository) {
-        this.roleRepository = roleRepository;
-    }
+    Role findRoleByName(String name);
 
-    public List<Role> getListRoles() {
-        return roleRepository.findAll();
-    }
-
-    public Role findRoleByName(String name) {
-        return roleRepository.findRoleByName(name);
-    }
-
-    public void saveRole(Role role) {
-        roleRepository.save(role);
-    }
+    @Transactional
+    void saveRole(Role role);
 }
--- src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
@@ -1,12 +1,10 @@
 package ru.kata.spring.boot_security.demo.services;
 
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Service;
 import ru.kata.spring.boot_security.demo.models.User;
 
 import java.util.List;
 
-@Service
 public interface UserService {
 
     User findUserByUsername(String username);
